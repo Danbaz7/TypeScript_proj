6b53821ac2ed4c3074d72977317c5fefb33df825
Daniel Obazee <obazeedaniel03@gmail.com>
2026-01-15 08:23:14 -0800

    - Introduced ProjectItem component to encapsulate the UI and logic for displaying individual project entries.

--- src/app.ts
+++ src/app.ts
@@ -158,7 +158,32 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 // class project item
-class ProjectItem {}
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  get persons() {
+    if (this.project.people === 1) {
+      return "1 person";
+    } else {
+      return `${this.project.people} persons`;
+    }
+  }
+
+  configure(): void {}
+  renderContent(): void {
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
+    this.element.querySelector("p")!.textContent = this.project.description;
+  }
+}
 
 // ProjectList class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
@@ -200,9 +225,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     listEl.innerHTML = "";
 
     for (const prjItem of this.assignedProjects) {
-      const listItem = document.createElement("li");
-      listItem.textContent = prjItem.title;
-      listEl.appendChild(listItem);
+      new ProjectItem(this.element.querySelector("ul")!.id, prjItem);
     }
   }
 }
